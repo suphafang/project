644bfe93bf83d9a50810d892d941c319fffc789b
suphafang <71120866+suphafang@users.noreply.github.com>
2020-12-16 14:07:29 +0700

    New

--- main.ino
+++ main.ino
@@ -2,11 +2,11 @@
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <FS.h>
+#include <Wire.h>
 
-int trig = D1;
-int echo = D2;
-
-HCSR04 hc(trig,echo);
+HCSR04 sensor1(D0,D1);
+HCSR04 sensor2(D3,D2);
 
 const char* ssid = "Chuensooksri_2.4G";
 const char* password = "178f420c";
@@ -14,16 +14,21 @@ const char* password = "178f420c";
 AsyncWebServer server(80);
 
 String getDistance() {
-    float d = hc.dist();
-    return String(d);
+    float d_1 = sensor1.dist();
+    float d_2 = sensor2.dist();
+    return "{\"sensor_1\":\""+String(d_1)+"\",\"sensor_2\":\""+String(d_2)+"\"}";
   }
 
 void setup() {
   // put your setup code here, to run once:
   {
-    Serial.begin(9600);
+    Serial.begin(115200);
   }
-  
+    // Initialize SPIFFS
+  if (! SPIFFS.begin ()) {
+    Serial.println ("An Error has occurred while mounting SPIFFS");
+    return;
+  }
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
       delay(1000);
@@ -32,10 +37,18 @@ void setup() {
 
   Serial.println(WiFi.localIP());
   
+  // Route for web page
+  server.on ("/", HTTP_GET, [] (AsyncWebServerRequest * request) {
+    request-> send (SPIFFS, "/index.html");
+  });
+  server.on ("/beta.html", HTTP_GET, [] (AsyncWebServerRequest * request) {
+    request-> send (SPIFFS, "/beta.html");
+  });
   server.on ("/distance", HTTP_GET, [] (AsyncWebServerRequest * request) {
     request-> send_P (200, "text / plain", getDistance(). c_str ());
   });
-
+  
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
   server.begin ();
 }
 
